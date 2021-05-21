bc257f423a0a5b580559c94ece81a6e83e4c20bb
unknown <chinh1995@gmail.com>
2021-05-21 10:37:22 +0700

    bai15

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const userRoute = require('./routes/user.route')
+const cookieParser = require('cookie-parser')
+const authRoute = require('./routes/auth.route')
+const authMiddleware = require('./middlewares/auth.middleware')
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -10,7 +13,7 @@ app.set('views', './views')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static('public'))
-
+app.use(cookieParser())
 
 // Routes
 app.get('/', (req, res) => {
@@ -18,7 +21,8 @@ app.get('/', (req, res) => {
 })
 
 
-app.use('/users', userRoute)
+app.use('/users', authMiddleware.requireAuth, userRoute)
+app.use('/auth/', authRoute)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,14 +1,17 @@
 var express = require('express')
 var router = express.Router()
 var controller = require('../controllers/user.controller')
+var validate = require('../validate/user.validate')
+const authMiddleware = require('../middlewares/auth.middleware')
 
-router.get('/', controller.index)
+
+router.get('/', authMiddleware.requireAuth, controller.index)
 
 router.get('/search', controller.search)
 
 router.get('/create', controller.create)
 
-router.post('/create', controller.postCreate)
+router.post('/create', validate.postCreate, controller.postCreate)
 
 router.get('/:id', controller.get)
 
